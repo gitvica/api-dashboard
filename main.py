f9df1911b80b096e51a2d68e726c162ff86cad51
V A <va@macbookpro.lan>
2025-05-23 00:14:02 -0500

    Initial commit for calling bitcoin price

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+def fetch_bitcoin_price():
+    url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+    response = requests.get(url)
+    data = response.json()
+    price = data["bitcoin"]["usd"]
+    print(f"Current Bitcoin Price: ${price}")
+
+if __name__ == "__main__":
+    fetch_bitcoin_price()
